e308aacc752334564aa2bebb4c2d4b0c74d939fc
patrykgolan <patrykgolan@gmail.com>
2023-04-16 11:48:48 +0100

    ToDoStatus Model attributes and migration

--- app/Models/ToDoTaskStatus.php
+++ app/Models/ToDoTaskStatus.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ToDoTaskStatus extends Model
 {
+    protected $fillable = [
+      'title',
+    ];
 
+    protected $casts = [
+        'title' => 'string'
+    ];
 }
--- database/migrations/2023_04_16_093559_create_to_do_task_statuses_table.php
+++ database/migrations/2023_04_16_093559_create_to_do_task_statuses_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ToDoTaskStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,17 @@ return new class extends Migration
     {
         Schema::create('to_do_task_statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
+
+        // create default statuses
+        $taskStatuses = ['To Do', 'Completed'];
+        foreach ($taskStatuses as $taskStatus){
+            ToDoTaskStatus::firstOrCreate([
+                'title' => $taskStatus
+            ]);
+        }
     }
 
     /**

a0eca77d075039e828a59f5c6f1a64c91671c59d
patrykgolan <patrykgolan@gmail.com>
2023-04-16 18:14:16 +0100

    analytics view changes

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -3,26 +3,30 @@
     Analytics
 @endsection
 @section('content')
-
-    <form method="GET" action="{{route('todo.analytics')}}">
-        <div class="d-flex">
-            <div class="d-flex flex-column" >
-                <label for="name">From</label>
-                <input class="form-control" name="from" type="date" placeholder="from" required value="{{$from}}">
+    <h1 class="mt-4 text-center">Analytics</h1>
+    <section class="mt-4">
+        <form method="GET" action="{{route('todo.analytics')}}">
+            <div class="d-flex">
+                <div class="d-flex flex-column" >
+                    <label for="name">From</label>
+                    <input class="form-control" name="from" type="date" placeholder="from" required value="{{$from}}">
+                </div>
+                <div class="d-flex flex-column">
+                    <label for="name">To</label>
+                    <input class="form-control"  name="to" type="date" placeholder="from" required value="{{$to}}">
+                </div>
+                <div >
+                    <button class="btn btn-primary mt-4" type="submit">
+                        View
+                    </button>
+                </div>
             </div>
-            <div class="d-flex flex-column">
-                <label for="name">To</label>
-                <input class="form-control"  name="to" type="date" placeholder="from" required value="{{$to}}">
-            </div>
-            <div >
-                <button class="btn btn-primary mt-4" type="submit">
-                    View
-                </button>
-            </div>
-        </div>
-    </form>
+        </form>
+    </section>
+    <section class="mt-4">
     <h3>Completed Tasks: {{$completed}}</h3>
     <h3>Created Tasks: {{$created}}</h3>
+    </section>
 @endsection
 
 
